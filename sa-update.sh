aac421e4aba357387bb7d79874681d86b1111b42
Arkadiusz Miśkiewicz <arekm@maven.pl>
2008-08-29 08:20:13 +0000

    - channels file support
    Changed files:
        sa-update.sh -> 1.2

--- sa-update.sh
+++ sa-update.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+OPT=""
+
+[ -f /etc/mail/spamassassin/channels ] && OPT="$OPT --channelfile /etc/mail/spamassassin/channels"
+
 sleep $(expr $RANDOM % 7200)
 # Only restart spamd if sa-update returns 0, meaning it updated the rules
-/usr/bin/sa-update || exit $?
+/usr/bin/sa-update $OPT || exit $?
 
 if [ -x /usr/bin/sa-compile ]; then
 	out=$(/usr/bin/sa-compile 2>&1)
@@ -13,6 +17,6 @@ if [ -x /usr/bin/sa-compile ]; then
 	fi
 fi
 
-if [ -x /etc/rc.d/init.d/spamd ]; then
+if [ -x /etc/rc.d/init.d/spamd -a -e /var/lock/subsys/spamd ]; then
 	/sbin/service spamd restart > /dev/null
 fi
